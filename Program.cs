b5fc9fb48b021e1d0ce73cbf7f693b68af7c8c46
den-ghost@mail.ru <den-ghost@mail.ru>
2022-07-06 19:51:25 +0300

    Homework 4 sem

--- Program.cs
+++ Program.cs
@@ -1,37 +1,27 @@
-﻿// Задача 5  Напишите программу, которая принимает на вход трехзначное число и на выходе показывает вторую цифру этого числа.
+﻿
+Console.WriteLine("Task 3");
+Console.Write("Inter Digit: ");
+int cube = Convert.ToInt32(Console.ReadLine());
 
-Console.Clear();
-Console.WriteLine("TASK 5 ");
-Console.Write("INTER THREE DIGIT NUMBER: ");
-int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
-string stringNumber = Convert.ToString(threeDigitNumber);
-Console.WriteLine("SECOND Digit -> "+stringNumber[1]);
-
-Console.WriteLine("TASK 13 ");
-
-Console.Write("INTER DIGIT: ");
-int anyNumber = Convert.ToInt32(Console.ReadLine());
-string anyNumberText = Convert.ToString(anyNumber);
-if (anyNumberText.Length > 2){
-  Console.WriteLine("Third digit -> " + anyNumberText[2]);
-}
-else {
-  Console.WriteLine("-> No third digit");
-}
-
-//Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
-
-Console.Write("Inter digit which means day of the week: ");
-int dayNumber = Convert.ToInt32(Console.ReadLine());
-
-void CheckingTheDayOfTheWeek (int dayNumber) {
-  if (dayNumber == 6 || dayNumber == 7) {
-  Console.WriteLine("(Dayoff) -> yes");
+void Cube(int[] cube)
+{
+  int counter = 0;
+  int length = cube.Length;
+  while (counter <  length){
+    cube[counter] = Convert.ToInt32(Math.Pow(counter, 3));
+    counter++;
   }
-  else if (dayNumber < 1 || dayNumber > 7) {
-    Console.WriteLine("There is no day of the week");
-  }
-  else Console.WriteLine("(There is not a dayoff) -> No");
 }
 
-CheckingTheDayOfTheWeek(dayNumber);
+void PrintArry(int[] coll){
+  int count = coll.Length;
+  int index = 0;
+  while(index < count){
+    Console.Write(coll[index]+ " ");
+    index++;
+  }
+} 
+
+int[] arry = new int[cube+1];
+Cube(arry);
+PrintArry(arry);
