677363a0d8bde2b90db63fbd47f3866f5f61058c
den-ghost@mail.ru <den-ghost@mail.ru>
2022-07-18 19:29:58 +0300

    cleaning

--- Program.cs
+++ Program.cs
@@ -1,27 +1,26 @@
-﻿
-Console.WriteLine("Task 3");
-Console.Write("Inter Digit: ");
-int cube = Convert.ToInt32(Console.ReadLine());
+﻿//Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементом массива.
+//[3 7 22 2 78] -> 76
 
-void Cube(int[] cube)
-{
-  int counter = 0;
-  int length = cube.Length;
-  while (counter <  length){
-    cube[counter] = Convert.ToInt32(Math.Pow(counter, 3));
-    counter++;
+double[] arrayRealNumbers = new double[10];
+  for (int i = 0; i < arrayRealNumbers.Length; i++ )
+  {
+    arrayRealNumbers[i] = new Random().Next(1, 10);
+    Console.Write(arrayRealNumbers[i] + " ");
   }
-}
 
-void PrintArry(int[] coll){
-  int count = coll.Length;
-  int index = 0;
-  while(index < count){
-    Console.Write(coll[index]+ " ");
-    index++;
+double maxNumber = arrayRealNumbers[0];
+double minNumber = arrayRealNumbers[0];
+
+  for (int i = 1; i < arrayRealNumbers.Length; i++)
+  {
+    if (maxNumber < arrayRealNumbers[i])
+    {
+      maxNumber = arrayRealNumbers[i];
+    }
+        if (minNumber > arrayRealNumbers[i])
+    {
+      minNumber = arrayRealNumbers[i];
+    }
   }
-} 
-
-int[] arry = new int[cube+1];
-Cube(arry);
-PrintArry(arry);
+  double decision = maxNumber - minNumber;
+  Console.WriteLine($"diff between max ({maxNumber}) and min ({minNumber}) elements: {decision}");
